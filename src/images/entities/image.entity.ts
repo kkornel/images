ac75c7e9a0d60ebf4d9d6ce47bdbdbd2df74a662
kkornel <kornel@kornel.com>
2026-04-11 05:39:54 +0200

    created image orm entity

---
+++ src/images/entities/image.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'images' })
+export class ImageOrmEntity {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  storageKey: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  mimeType: string;
+
+  @Column({ type: 'varchar', length: 10 })
+  extension: string;
+
+  @Column({ type: 'integer' })
+  width: number;
+
+  @Column({ type: 'integer' })
+  height: number;
+
+  @Column({ type: 'integer' })
+  size: number;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
+import { ImageOrmEntity } from './entities/image.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ImageOrmEntity])],
   providers: [ImagesService],
   controllers: [ImagesController],
 })
